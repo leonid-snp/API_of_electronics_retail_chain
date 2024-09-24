f1f5bc5904a536e2dbc05289b65691da8d0aad42
leonid Petrov <leonidpetrov637@gmail.com>
2024-09-24 08:35:55 +0300

    added
    created models Factory, Retail Network, Individual Entrepreneur, Contacts, Products, and added environment variables to the project using the python-dotenv library

---
+++ electronics_network/models.py
@@ -0,0 +1,159 @@
+from django.db import models
+from config.settings import NULLABLE
+
+
+class Contact(models.Model):
+    """
+    Модель `Контакты` - контакты субъектов.
+    """
+    email = models.EmailField(
+        unique=True,
+        max_length=150,
+        verbose_name='E-mail',
+        help_text='Введите e-mail'
+    )
+    country = models.CharField(
+        max_length=60,
+        verbose_name='Страна',
+        help_text='Введите страну',
+        **NULLABLE
+    )
+    city = models.CharField(
+        max_length=60,
+        verbose_name='Город',
+        help_text='Введите город',
+        **NULLABLE
+    )
+    street = models.CharField(
+        max_length=60,
+        verbose_name='Улица',
+        help_text='Введите улицу',
+        **NULLABLE
+    )
+    house = models.PositiveIntegerField(
+        verbose_name='Номер дома',
+        help_text='Введите номер дома',
+        **NULLABLE
+    )
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'Контакты'
+
+
+class Product(models.Model):
+    """
+    Модель `Продукты` - продукты субъекта.
+    """
+    name = models.CharField(
+        max_length=60,
+        verbose_name='Название',
+        help_text='Введите название'
+    )
+    model = models.CharField(
+        max_length=60,
+        verbose_name='Модель',
+        help_text='Введите модель',
+        **NULLABLE
+    )
+    release = models.DateField(
+        verbose_name='Дата выхода',
+        help_text='Введите дату выхода на рынок',
+        **NULLABLE
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+
+class DataMixin(models.Model):
+    """
+    Модель `Данные субъекта`.
+    """
+    name = models.CharField(
+        max_length=60,
+        verbose_name='Название',
+        help_text='Введите название'
+    )
+    contacts = models.OneToOneField(
+        Contact,
+        on_delete=models.CASCADE,
+        verbose_name='Контакты',
+        help_text='Введите контакты'
+    )
+    products = models.ManyToManyField(
+        Product,
+        verbose_name='Продукты',
+        help_text='Введите продукты'
+    )
+    dept = models.PositiveIntegerField(
+        verbose_name='Задолженность',
+        help_text='Введите задолженность перед поставщиком',
+        **NULLABLE
+    )
+    creation_time = models.DateTimeField(
+        verbose_name='Время',
+        help_text='Введите время создания',
+        **NULLABLE
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Entrepreneur(DataMixin):
+    """
+    Модель `Индивидуальный предприниматель`.
+    """
+    supplier = models.ForeignKey(
+        'RetailChain',
+        on_delete=models.SET_NULL,
+        verbose_name='Поставщик',
+        help_text='Введите поставщика',
+        **NULLABLE
+    )
+
+    class Meta:
+        verbose_name = 'Предприниматель'
+        verbose_name_plural = 'Предприниматели'
+
+
+class RetailChain(DataMixin):
+    """
+    Модель `Розничная сеть`.
+    """
+    supplier = models.ForeignKey(
+        Entrepreneur,
+        on_delete=models.SET_NULL,
+        verbose_name='Поставщик',
+        help_text='Введите поставщика',
+        **NULLABLE
+    )
+
+    class Meta:
+        verbose_name = 'Розничная сеть'
+        verbose_name_plural = 'Розничные сети'
+
+
+class Factory(DataMixin):
+    """
+    Модель `Завод` - завод производитель.
+    """
+    supplier = models.ForeignKey(
+        RetailChain,
+        on_delete=models.SET_NULL,
+        verbose_name='Поставщик',
+        help_text='Введите поставщика',
+        **NULLABLE
+    )
+
+    class Meta:
+        verbose_name = 'Завод'
+        verbose_name_plural = 'Заводы'
