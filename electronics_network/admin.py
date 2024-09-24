ef177ac965d576dac8768d3aae7cde6d691c13a7
leonid Petrov <leonidpetrov637@gmail.com>
2024-09-24 13:16:52 +0300

    added
    added display of models in the admin panel

--- electronics_network/admin.py
+++ electronics_network/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from electronics_network.models import Factory, RetailChain, Entrepreneur
+
+
+@admin.register(Factory)
+class FactoryAdmin(admin.ModelAdmin):
+    """
+    Класс `Завод` - отображения в админ-панели.
+    """
+    list_display = ('id', 'name', 'contacts', 'dept', 'creation_time')
+    list_filter = ('contacts',)
+
+
+@admin.register(RetailChain)
+class RetailChainAdmin(admin.ModelAdmin):
+    """
+    Класс `Розничная сеть` - отображение в админ-панели.
+    """
+    list_display = ('id', 'name', 'contacts', 'dept', 'creation_time')
+    list_filter = ('contacts',)
+
+
+@admin.register(Entrepreneur)
+class EntrepreneurAdmin(admin.ModelAdmin):
+    """
+    Модель `Предприниматель` - отображение в админ-панели.
+    """
+    list_display = ('id', 'name', 'contacts', 'dept', 'creation_time')
+    list_filter = ('contacts',)
